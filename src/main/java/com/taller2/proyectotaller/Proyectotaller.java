42683ea8d8d72f2cf2537163c4965fe0062a48b2
FranSagasti2609 <fransagasti2609@gmail.com>
2025-05-03 11:44:32 -0300

    Estructura con maven

---
+++ src/main/java/com/taller2/proyectotaller/Proyectotaller.java
@@ -0,0 +1,8 @@
+package com.taller2.proyectotaller;
+
+public class Proyectotaller {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World de taller 2!");
+    }
+}
